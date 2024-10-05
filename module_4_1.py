584b77f0bca1c8274992abb37009085d3c3bc4af
bodobondit <bodo93@gmail.com>
2024-10-05 14:18:01 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from True_math import divide as T_div
+from Fake_math import divide as F_div
+
+result1 = F_div(69, 3)
+result2 = F_div(3, 0)
+result3 = T_div(49, 7)
+result4 = T_div(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
