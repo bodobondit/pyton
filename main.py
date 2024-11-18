fa873a4bfd3dc4fb1d9dd2415eba61d902b80847
bodobondit <bodo93@gmail.com>
2024-11-18 20:39:08 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2nd program
+q = 9.99 > 9.98 and 1000 != 1000.1
+print(q)
+
+# 3rd program
+a = 2 * 2 + 2
+print(a)
+b = 2 * (2 + 2)
+print(b)
+print(a == b)
+
+# 4th program
+s = '123.456'
+s = float(s) * 10
+s = int(s) % 10
+print(s)
